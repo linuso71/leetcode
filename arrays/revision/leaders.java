7130130993f7dd5ce3803d963979ad766bfb18f6
linuso77 <dharmenderchugh71@gmail.com>
2024-08-26 03:23:54 +0530

    practice questions

---
+++ arrays/revision/leaders.java
@@ -0,0 +1,9 @@
+public class leaders {
+    public static void main(String[] args) {
+        int[] arr = {16,17,4,3,5,2};
+        int[] leaders;  
+        for(int i=0;i<arr.length;i++){
+
+        }
+    }
+}
