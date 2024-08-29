fbf49f6a3174f9b52338ab5cc3ed9c0432eeaf1e
linuso77 <dharmenderchugh71@gmail.com>
2024-08-29 20:53:26 +0530

    optimal approach

--- arrays/longest_consecutive_128.java
+++ arrays/longest_consecutive_128.java
@@ -1,24 +1,26 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 public class longest_consecutive_128 {
     public static void main(String[] args) {
-        int[] arr = {100,4,200,1,3,2};
-        merge_sort(arr, 0, arr.length-1);
-        int longest = 1;
-        int last = Integer.MAX_VALUE;
-        int count = 0;
-        for (int i=0;i<arr.length;i++){
-            if(arr[i] -1 == last){
-                count +=1;
-                last = arr[i];
-            }
-            else if(arr[i] != last){
-                count = 1;
-                last = arr[i];
-            }
-            longest = Math.max(longest, count);
-        }
-        System.out.println(longest);
+        int[] arr = {100,4,200,1,3,2,4,5,101,102,103,104,105};
+        System.out.println(optimal_sol(arr));
+        // merge_sort(arr, 0, arr.length-1);
+        // int longest = 1;
+        // int last = Integer.MAX_VALUE;
+        // int count = 0;
+        // for (int i=0;i<arr.length;i++){
+        //     if(arr[i] -1 == last){
+        //         count +=1;
+        //         last = arr[i];
+        //     }
+        //     else if(arr[i] != last){
+        //         count = 1;
+        //         last = arr[i];
+        //     }
+        //     longest = Math.max(longest, count);
+        // }
+        // System.out.println(longest);
     } 
     static void merge_sort(int[] arr,int low,int high){
         if (low>= high){
@@ -56,5 +58,25 @@ public class longest_consecutive_128 {
             arr[i] = new_arr.get(i-low);
         }
 
+    }
+    static int optimal_sol(int[] arr){
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+        for(int it:set){
+            if (!set.contains(it-1)){
+                int cnt = 1;
+                int x = it;
+                while(set.contains(x+1)){
+                    x += 1;
+                    cnt +=1;
+                }
+                longest = Math.max(longest, cnt);
+
+            }
+        }
+        return longest;
     }
 }
